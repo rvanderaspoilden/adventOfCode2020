ff68a42cf5de710661db51734055c8168b03e507
Rodolphe <rodolphevanderaspoilden@outlook.fr>
2020-12-02 19:11:28 +0100

    :sparkles: day 2 - part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -6,25 +6,22 @@ namespace Day2 {
     internal class Program {
         private const char SPACE = ' ';
         private const char TIRET = '-';
-        private const char COLON = ':';
-        
+
         public static void Main(string[] args) {
             string[] lines = File.ReadAllLines("../../Input/input.txt");
             int validCounter = 0;
-            
-            Console.WriteLine(lines.Length);
 
             foreach (string line in lines) {
                 string[] parts = line.Split(SPACE);
-                int min = int.Parse(parts[0].Split(TIRET)[0]);
-                int max = int.Parse(parts[0].Split(TIRET)[1]);
-                char charToFind = parts[1].ToCharArray()[0];
-                char[] passwordChars = parts[2].ToCharArray();
 
-                int occurences = passwordChars.ToList().Where(x => x.Equals(charToFind)).ToArray().Length;
-                validCounter += occurences >= min && occurences <= max ? 1 : 0;
+                int[] expectedPositions = parts[0].Split(TIRET).ToList().Select(int.Parse).ToArray();
+
+                char charToFind = parts[1].ToCharArray()[0];
+                string password = parts[2];
+
+                validCounter += (expectedPositions.Count(pos => password[pos - 1] == charToFind) == 1) ? 1 : 0;
             }
-            
+
             Console.WriteLine("result is : " + validCounter);
         }
     }
