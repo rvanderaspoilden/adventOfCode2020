c499a1fcc99263005345aa25a78b9e47789821fa
Rodolphe <rodolphevanderaspoilden@outlook.fr>
2020-12-06 22:21:27 +0100

    :sparkles: day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6 {
+    class Program {
+        static void Main(string[] args) {
+            string[] lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "../../../Input/input.txt"));
+            HashSet<char> groupAnswers = new HashSet<char>();
+            int count = 0;
+            bool takeFirst = true;
+
+            foreach (string line in lines) {
+                if (line.Length == 0) { // End of group
+                    count += groupAnswers.Count;
+                    groupAnswers.Clear();
+                    takeFirst = true;
+                } else {
+                    if (takeFirst) {
+                        line.ToCharArray().ToList().ForEach(answer => groupAnswers.Add(answer));
+                        takeFirst = false;
+                    } else {
+                        HashSet<char> answers = line.ToCharArray().ToHashSet();
+                        groupAnswers.IntersectWith(answers);
+                    }
+                }
+            }
+
+            count += groupAnswers.Count;
+            groupAnswers.Clear();
+
+
+            Console.WriteLine("Answers : " + count);
+        }
+    }
+}
